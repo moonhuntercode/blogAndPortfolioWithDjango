7caf3d1e58b839f142bbe63dff08399c0c0e8a24
moonhunter <vdcloayza@gmail.com>
2021-08-19 23:08:36 -0400

    tag templates added

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.portfolio,name="portfolio"),
-    path('',views.about,name="about"),
+    path('about/',views.about,name="about"),
+    path('contact/',views.contact,name="contact")
 ]
